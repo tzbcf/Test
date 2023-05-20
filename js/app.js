21d662ebb3c9a795c522f1aded06b5f3e4722e7d
陈志强 <chenzhiqiang@wasion.com>
2023-05-20 15:26:37 +0800

    test

--- js/app.js
+++ js/app.js
@@ -12,18 +12,6 @@
  *
  * @format
  */
-/*****
- * Init Project
- */
-function init(url) {
-
-  /* ---------- Tooltip ---------- */
-  $('[rel="tooltip"],[data-rel="tooltip"]').tooltip({"placement":"bottom",delay: { show: 400, hide: 200 }});
-
-  /* ---------- Popover ---------- */
-  $('[rel="popover"],[data-rel="popover"],[data-toggle="popover"]').popover();
-
-}
 
 /*****
  * ASYNC LOAD
@@ -33,15 +21,14 @@ function loadJS(jsFiles) {
 
   const body = document.getElementsByTagName('body')[0];
 
-  for(var i = 0; i<jsFiles.length; i++){
+  for(let i = 0; i<jsFiles.length; i++){
       appendScript(body, jsFiles[i])
   }
 
-  init();
 }
 
 function appendOnce(element, script) {
-  var scripts = Array.from(document.querySelectorAll('script')).map(function(scr){return scr.src;});
+  const scripts = Array.from(document.querySelectorAll('script')).map(function(scr){return scr.src;});
 
   if (!scripts.includes(script.src)) {
       element.appendChild(script)
@@ -50,7 +37,7 @@ function appendOnce(element, script) {
 
 function appendScript(element, src) {
   const async = (src.substring(0, 4) === 'http');
-  var script = document.createElement('script');
+  const script = document.createElement('script');
   script.type = 'text/javascript';
   script.async = async;
   script.defer = async;
